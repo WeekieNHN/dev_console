74cb840d4b6f13dea2135589d8355da626fbe955
Frank Podraza <frankpodraza@icloud.com>
2024-12-28 18:56:52 -0800

    Added Add-On Files

---
+++ addons/dev_console/Autoloads/console_manager.gd
@@ -0,0 +1,54 @@
+extends Node
+
+# Use this to grab focus when the console is closed
+signal on_console_close
+
+@export var show_prompt: bool = true
+@onready var prompt_layer: Control = $PromptLayer
+
+@onready var console_layer: CanvasLayer = $ConsoleCanvasLayer
+@onready var dev_console: DevConsole = %DevConsole
+@onready var commands_holder: Node = %Commands
+
+var command_dict = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Hide the console
+	console_layer.visible = false
+	# Show/hide the prompt
+	prompt_layer.visible = show_prompt
+	# Add commands
+	for child in commands_holder.get_children():
+		if child is Command:
+			ConsoleManager.add_command(child.command, child)
+
+func add_command(cmd_name: String, cmd: Command) -> void:
+	# Add command to dict
+	command_dict[cmd_name] = cmd
+
+func _input(_event):
+	# Check if console input was pressed, if so, toggle
+	if Input.is_action_just_pressed("console"):
+		toggle_visible()
+
+func toggle_visible() -> void:
+	# Toggle the visibility
+	console_layer.visible = !console_layer.visible
+	# Emit closed signal
+	if !console_layer.visible: on_console_close.emit()
+
+func run_command(raw_command: String) -> void:
+	# Split the command
+	var command_split = raw_command.split(' ', false)
+	# Identify Command
+	if command_dict.has(command_split[0]):
+		# Print the command to console to indicate it was submitted
+		dev_console.console_print_log("> %s\n" % raw_command)
+		# Run the command
+		var output_string: String = command_dict[command_split[0]].run_command(command_split.slice(1))
+		# Print it's output to dev_console 
+		dev_console.console_print_log(output_string)
+	# If unidentified, tell the user
+	else:
+		dev_console.console_print_log("Command `%s` not found\n" % command_split[0])
---
+++ addons/dev_console/Commands/command.gd
@@ -0,0 +1,8 @@
+class_name Command extends Node
+
+@export var command: String = "help"
+@export var help_text: String = ""
+@export var help_text_short: String = ""
+
+func run_command(args: Array[String]) -> String:
+	return ""
